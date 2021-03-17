f2311d16059654e3ce4706b65ea133a5d11bca57
simonzg <zhanghan.simon@gmail.com>
2021-03-17 11:46:12 +0800

    add deploy script for mainnet_newapi

---
+++ deploy_mainnet_newapi.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# build
+NODE_ENV=production npm run build
+
+# copy dist to mainnet S3
+aws s3 cp ./dist/ s3://scanv2-main-newapi/ --recursive --acl public-read
+
+# invalidate mainnet cloudfront cache
+# aws cloudfront create-invalidation --distribution-id E2BSH8388R89SO --paths "/*"
+
