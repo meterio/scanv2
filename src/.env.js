018833cb93aa93285ce465d0865b80b70826bdac
kda-data <kda-data@protonmail.com>
2022-01-11 19:26:29 +0800

    update sdk-beta testnet api

--- src/.env.js
+++ src/.env.js
@@ -1,4 +1,4 @@
 export const API_PORT = "3456";
-export const API_URL = `http://18.141.219.204:${API_PORT}`
+export const API_URL = `http://172.31.4.237:${API_PORT}`
 // export const API_URL = `https://jsonplaceholder.typicode.com`;
 export const DOMAIN_TITLE = "Verse Network";
