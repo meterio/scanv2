f4dfa8caeb9d8f5ffb331ea44ca3f98bf9e48bcf
Simon.Z <zhanghan.simon@gmail.com>
2023-03-07 09:02:49 -0800

    rearrange epoch stats page

--- src/pages/EpochStats.vue
+++ src/pages/EpochStats.vue
@@ -1,59 +1,63 @@
 <template>
   <b-container>
-    <div v-if="loading" class="position-relative epoch-loading">
-      <Loading />
-    </div>
-    <div v-else class="detail-page">
-      <section class="d-flex justify-content-center title-row">
-        <!-- <b-link class="link" :to="{ name: 'epochStats', params: { epoch: Number(epoch) - 1 } }">&lt;&lt;</b-link> -->
-        Statistics for Epoch: {{ epoch }}
-        <!-- <b-link class="link" :to="{ name: 'epochStats', params: { epoch: Number(epoch) + 1 } }"> &gt;&gt; </b-link> -->
-      </section>
-      <section class="d-flex justify-content-between flex-wrap">
-        <span class="p-1 rounded" v-for="(value, key, indx) in detail" :key="indx"> {{ key }}: {{ value }} </span>
-      </section>
-      <section class="row">
-        <div class="col-md-4"><span class="fail">F</span> - failed round, no block produced</div>
-        <div class="col-md-4"><span class="success">B</span> - success round producing mBlock <br /></div>
-        <div class="col-md-4"><span class="success">KB</span> - success round producing KBlock<br /></div>
-        <div class="col-md-4"><span class="success delay-1m">B</span> - success round, with > 1min delay<br /></div>
-        <div class="col-md-4"><span class="success delay-20s">B</span> - success round, with > 20s delay<br /></div>
-        <div class="col-md-4"><span class="success delay-5s">B</span> - success round, with > 5s delay<br /></div>
-      </section>
-      <section class="d-flex flex-wrap mt-2">
-        <span v-for="(summary, round) in stats" :key="round">
-          <span
-            :style="{ cursor: 'pointer' }"
-            :id="'round-' + round"
-            :title="memberAt(round).name"
-            :class="
-              (summary.status == 1 ? 'success' : 'fail') +
-              ' ' +
-              (summary.intvl > 60
-                ? 'delay-1m'
-                : summary.intvl > 20
-                ? 'delay-20s'
-                : summary.intvl > 5
-                ? 'delay-5s'
-                : 'no-delay')
-            "
-            >&nbsp;
-            {{ summary.status == 1 ? (summary.k ? 'KB' : 'B') : 'F' }}
+    <b-card class="stats">
+      <div v-if="loading" class="position-relative epoch-loading">
+        <Loading />
+      </div>
+      <div v-else>
+        <section class="d-flex justify-content-center title-row">
+          <!-- <b-link class="link" :to="{ name: 'epochStats', params: { epoch: Number(epoch) - 1 } }">&lt;&lt;</b-link> -->
+          Statistics for Epoch: {{ epoch }}
+          <!-- <b-link class="link" :to="{ name: 'epochStats', params: { epoch: Number(epoch) + 1 } }"> &gt;&gt; </b-link> -->
+        </section>
+
+        <section class="row mt-1">
+          <div class="col-md-4"><span class="success">B</span> success round producing mBlock<br /></div>
+          <div class="col-md-4"><span class="success">KB</span> success round producing KBlock<br /></div>
+          <div class="col-md-4"><span class="fail">F</span> failed round, no block produced</div>
+          <div class="col-md-4"><span class="success delay-5s">B</span> success round, delays 5-20s<br /></div>
+          <div class="col-md-4"><span class="success delay-20s">B</span> success round, delays 20-60s<br /></div>
+          <div class="col-md-4"><span class="success delay-1m">B</span> success round, delays &gt; 1min<br /></div>
+        </section>
+        <hr />
+        <section class="d-flex justify-content-between flex-wrap mb-1 hints">
+          <span class="p-1 rounded" v-for="(value, key, indx) in detail" :key="indx"> {{ key }}: {{ value }} </span>
+        </section>
+        <section class="d-flex flex-wrap">
+          <span v-for="(summary, round) in stats" :key="round">
+            <span
+              :style="{ cursor: 'pointer' }"
+              :id="'round-' + round"
+              :title="memberAt(round).name"
+              :class="
+                (summary.status == 1 ? 'success' : 'fail') +
+                ' ' +
+                (summary.intvl > 60
+                  ? 'delay-1m'
+                  : summary.intvl > 20
+                  ? 'delay-20s'
+                  : summary.intvl > 5
+                  ? 'delay-5s'
+                  : 'no-delay')
+              "
+              >&nbsp;
+              {{ summary.status == 1 ? (summary.k ? 'KB' : 'B') : 'F' }}
+            </span>
+            <b-tooltip :target="'round-' + round">
+              <div>Round: {{ round }}</div>
+
+              <div v-if="summary.status == 1">{{ summary.k ? 'K' : 'm' }}Block: {{ summary.b }}</div>
+              <div v-else>Status: timeout</div>
+
+              <div v-if="summary.status == 1">Used: {{ summary.intvl }} sec</div>
+
+              <div v-if="summary.status == 1">Proposer: {{ memberAt(round).name }}</div>
+              <div v-else>Expected: {{ memberAt(round).name }}</div>
+            </b-tooltip>
           </span>
-          <b-tooltip :target="'round-' + round">
-            <div>Round: {{ round }}</div>
-
-            <div v-if="summary.status == 1">{{ summary.k ? 'K' : 'm' }}Block: {{ summary.b }}</div>
-            <div v-else>Status: timeout</div>
-
-            <div v-if="summary.status == 1">Used: {{ summary.intvl }} sec</div>
-
-            <div v-if="summary.status == 1">Proposer: {{ memberAt(round).name }}</div>
-            <div v-else>Expected: {{ memberAt(round).name }}</div>
-          </b-tooltip>
-        </span>
-      </section>
-    </div>
+        </section>
+      </div>
+    </b-card>
   </b-container>
 </template>
 
@@ -70,9 +74,9 @@ export default {
       stats: [],
       memberMap: {},
       detail: {
-        nBlocks: '0',
+        Blocks: '0',
         'Committee Size': '0',
-        'Timeout Count': '0',
+        Timeouts: '0',
         Duration: '0',
       },
     };
@@ -86,9 +90,9 @@ export default {
 
     const { startBlock, endBlock, committeeSize, stats, members } = res;
     this.detail = {
-      nBlocks: endBlock.number - startBlock.number + 1,
+      Blocks: endBlock.number - startBlock.number + 1,
       'Committee Size': committeeSize,
-      'Timeout Count': stats.filter((item) => item.status !== 1).length,
+      Timeouts: stats.filter((item) => item.status !== 1).length,
       Duration: formatDuration(1000 * (endBlock.timestamp - startBlock.timestamp)),
     };
     this.stats = stats;
@@ -159,5 +163,15 @@ export default {
   font-size: 1.1rem;
   font-weight: bold;
   padding: 0.4rem 0 1rem;
+}
+hr {
+  margin-top: 0.5rem;
+  margin-bottom: 0.5rem;
+}
+.stats {
+  margin-top: 30px;
+}
+.hints {
+  font-size: 90%;
 }
 </style>
